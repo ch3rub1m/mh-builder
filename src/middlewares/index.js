f398d72c72afa094ef11d034ab61be5a505a1652
ch3rub1m <ch3rub1m@foxmail.com>
2017-03-31 00:50:02 +0800

    add redux-devtools-extension to project

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,6 @@
 import { applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import callAPI from './callAPI'
 
-export default applyMiddleware(thunk, callAPI)
+export default composeWithDevTools(applyMiddleware(thunk, callAPI))
